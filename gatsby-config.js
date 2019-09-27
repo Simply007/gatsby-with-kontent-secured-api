553d7357e8d98e165ecfc2e31cd5dd7f58793acc
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-09-27 13:32:51 +0200

    showcase

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,21 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-kentico-cloud`,
+      options: {
+        deliveryClientConfig: { // Configuration object
+          projectId: `6bf2c182-c337-010c-2383-ac4eddf0e261`,
+          enableSecuredMode: true,
+          securedApiKey: 'ew0KICAiYWxnIjogIkhTMjU2IiwNCiAgInR5cCI6ICJKV1QiDQp9.ew0KICAianRpIjogImE1ZjQ1NTdkN2U1ZDRhMDE4Y2YxOTMyZGIwM2VhODExIiwNCiAgImlhdCI6ICIxNTY5NTgyMjUwIiwNCiAgImV4cCI6ICIxOTE1MTgyMjUwIiwNCiAgInByb2plY3RfaWQiOiAiNmJmMmMxODJjMzM3MDEwYzIzODNhYzRlZGRmMGUyNjEiLA0KICAidmVyIjogIjEuMC4wIiwNCiAgImF1ZCI6ICJkZWxpdmVyLmtlbnRpY29jbG91ZC5jb20iDQp9.fqyqyx-NbyB8lrAX6uJhj1E-MJ5mfE8mI31wE37lFyA',
+          typeResolvers: [],
+          enableAdvancedLogging: true,
+        },
+        languageCodenames: [ // example configuration
+          `default`, // default language
+        ]
+      }
+    }
+  ]
 }
